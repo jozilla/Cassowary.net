31a13ac1ef0c7dde15fc44636ca2d614340e4cef
Jo Vermeulen <jo@lumumba.uhasselt.be>
2005-05-21 13:45:43 +0000

    Renamed Test.cs to ClTests.cs (and updated contents).

---
+++ ClTests.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Cassowary
+{
+	/// <summary>
+	/// Cassowary test class.
+	/// </summary>
+	class ClTests
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main(string[] args)
+		{
+		}
+	}
+}
