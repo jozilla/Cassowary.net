0a0682397767c2a8119e920c644af277b77e85a1
Jo Vermeulen <jo@lumumba.uhasselt.be>
2005-05-22 13:21:13 +0000

    Added ClLinearConstraint and updated TODO.

---
+++ ClLinearConstraint.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace Cassowary
+{
+	class ClLinearConstraint : ClConstraint
+	{
+		public ClLinearConstraint(ClLinearExpression cle,
+															ClStrength strength, 
+															double weight) : base(strength, weight)
+		{
+			_expression = cle;
+		}
+
+		public ClLinearConstraint(ClLinearExpression cle,
+															ClStrength strength) : base(strength, 1.0)
+		{		
+			_expression = cle;
+		}
+
+		public ClLinearConstraint(ClLinearExpression cle) : base(ClStrength.Required, 1.0)
+		{
+			_expression = cle;
+		}
+
+		public override ClLinearExpression Expression
+		{
+			get { return _expression; }
+		}
+
+		protected void SetExpression(ClLinearExpression expr)
+		{
+			_expression = expr;
+		}
+
+		protected ClLinearExpression _expression;
+	}
+}
