296e9352d46dd2ccd045f8afd5bec27d63923bc4
Jo Vermeulen <jo@lumumba.uhasselt.be>
2005-05-24 21:31:03 +0000

    Fixed access specifier of ClObjectiveVariable class.

--- Cassowary/ClObjectiveVariable.cs
+++ Cassowary/ClObjectiveVariable.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Cassowary
 {
-	class ClObjectiveVariable : ClAbstractVariable
+	public class ClObjectiveVariable : ClAbstractVariable
 	{
 		public ClObjectiveVariable(string name) : base(name)
 		{}
