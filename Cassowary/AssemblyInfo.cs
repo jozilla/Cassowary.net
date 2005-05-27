04a4560017b94dbfd70dcf065054ef564f631d85
Jo Vermeulen <jo@lumumba.uhasselt.be>
2005-05-27 14:45:15 +0000

    Preparing stuff for 0.1 release.

--- Cassowary/AssemblyInfo.cs
+++ Cassowary/AssemblyInfo.cs
@@ -6,8 +6,8 @@ using System.Runtime.CompilerServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 //
-[assembly: AssemblyTitle("")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Cassowary")]
+[assembly: AssemblyDescription("A constraint solving toolkit")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
@@ -26,7 +26,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.*")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
