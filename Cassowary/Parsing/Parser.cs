af5bec926f203a3a27f556f62ac3bbbaa163eed4
Jo Vermeulen <jo.vermeulen@uhasselt.be>
2007-05-09 16:07:37 +0200

    Added the Scanner and Parser files generated by Coco/R.

---
+++ Cassowary/Parsing/Parser.cs
@@ -0,0 +1,288 @@
+using System.Collections;
+using System.Globalization;
+using Cassowary;
+
+using System;
+
+
+
+public class Parser {
+	const int _EOF = 0;
+	const int _leq = 1;
+	const int _geq = 2;
+	const int _eq = 3;
+	const int _altleq = 4;
+	const int _altgeq = 5;
+	const int _plus = 6;
+	const int _minus = 7;
+	const int _times = 8;
+	const int _divide = 9;
+	const int _lparen = 10;
+	const int _rparen = 11;
+	const int _variable = 12;
+	const int _number = 13;
+	const int maxT = 14;
+
+	const bool T = true;
+	const bool x = false;
+	const int minErrDist = 2;
+	
+	public Scanner scanner;
+	public Errors  errors;
+
+	public Token t;    // last recognized token
+	public Token la;   // lookahead token
+	int errDist = minErrDist;
+
+ClConstraint _constraint;
+Hashtable _context = new Hashtable();
+
+public Hashtable Context
+{
+  get { return _context; }
+  set { _context = value; }
+}
+
+public ClConstraint Value
+{
+  get { return _constraint; }
+  set { _constraint = value; }
+}
+
+
+
+	public Parser(Scanner scanner) {
+		this.scanner = scanner;
+		errors = new Errors();
+	}
+
+	void SynErr (int n) {
+		if (errDist >= minErrDist) errors.SynErr(la.line, la.col, n);
+		errDist = 0;
+	}
+
+	public void SemErr (string msg) {
+		if (errDist >= minErrDist) errors.SemErr(t.line, t.col, msg);
+		errDist = 0;
+	}
+	
+	void Get () {
+		for (;;) {
+			t = la;
+			la = scanner.Scan();
+			if (la.kind <= maxT) { ++errDist; break; }
+
+			la = t;
+		}
+	}
+	
+	void Expect (int n) {
+		if (la.kind==n) Get(); else { SynErr(n); }
+	}
+	
+	bool StartOf (int s) {
+		return set[s, la.kind];
+	}
+	
+	void ExpectWeak (int n, int follow) {
+		if (la.kind == n) Get();
+		else {
+			SynErr(n);
+			while (!StartOf(follow)) Get();
+		}
+	}
+
+
+	bool WeakSeparator(int n, int syFol, int repFol) {
+		int kind = la.kind;
+		if (kind == n) {Get(); return true;}
+		else if (StartOf(repFol)) {return false;}
+		else {
+			SynErr(n);
+			while (!(set[syFol, kind] || set[repFol, kind] || set[0, kind])) {
+				Get();
+				kind = la.kind;
+			}
+			return StartOf(syFol);
+		}
+	}
+
+	
+	void Constraint() {
+		ClLinearExpression e1, e2; bool eq = false, geq = false, leq = false; 
+		Expression(out e1);
+		if (la.kind == 3) {
+			Get();
+			eq = true; 
+		} else if (la.kind == 2) {
+			Get();
+			geq = true; 
+		} else if (la.kind == 5) {
+			Get();
+			geq = true; 
+		} else if (la.kind == 1) {
+			Get();
+			leq = true; 
+		} else if (la.kind == 4) {
+			Get();
+			leq = true; 
+		} else SynErr(15);
+		Expression(out e2);
+		if (eq)
+		 Value = new ClLinearEquation(e1, e2);
+		else if (geq)
+		  Value = new ClLinearInequality(e1, Cl.GEQ, e2);
+		else if (leq)
+		  Value = new ClLinearInequality(e1, Cl.LEQ, e2);
+		
+	}
+
+	void Expression(out ClLinearExpression e) {
+		e = null; ClLinearExpression e1; 
+		Term(out e);
+		while (la.kind == 6 || la.kind == 7) {
+			if (la.kind == 6) {
+				Get();
+				Term(out e1);
+				e = Cl.Plus(e, e1); 
+			} else {
+				Get();
+				Term(out e1);
+				e = Cl.Minus(e, e1); 
+			}
+		}
+	}
+
+	void Term(out ClLinearExpression e) {
+		e = null; ClLinearExpression e1; 
+		Factor(out e);
+		while (la.kind == 8 || la.kind == 9) {
+			if (la.kind == 8) {
+				Get();
+				Factor(out e1);
+				e = Cl.Times(e, e1); 
+			} else {
+				Get();
+				Factor(out e1);
+				e = Cl.Divide(e, e1); 
+			}
+		}
+	}
+
+	void Factor(out ClLinearExpression e) {
+		e = null; ClDouble d; ClVariable v; bool negate = false; 
+		if (la.kind == 7) {
+			Get();
+			negate = true; 
+		}
+		if (la.kind == 13) {
+			Number(out d);
+			e = new ClLinearExpression(d.Value); 
+		} else if (la.kind == 12) {
+			Variable(out v);
+			e = new ClLinearExpression(v); 
+		} else if (la.kind == 10) {
+			Get();
+			Expression(out e);
+			Expect(11);
+		} else SynErr(16);
+		if (negate)
+		 e = Cl.Minus(0, e);
+		
+	}
+
+	void Number(out ClDouble d) {
+		Expect(13);
+		double tmpVal = double.Parse(t.val, new CultureInfo("en-US").NumberFormat);
+		d = new ClDouble(tmpVal);
+		
+	}
+
+	void Variable(out ClVariable v) {
+		Expect(12);
+		if (Context.ContainsKey(t.val))
+		{
+		  v = (ClVariable) Context[t.val];
+		}
+		else
+		{
+		SemErr("Undefined variable: " + t.val);
+		v = null;
+		                      }
+		                    
+	}
+
+
+
+	public void Parse() {
+		la = new Token();
+		la.val = "";		
+		Get();
+		Constraint();
+
+    Expect(0);
+	}
+	
+	bool[,] set = {
+		{T,x,x,x, x,x,x,x, x,x,x,x, x,x,x,x}
+
+	};
+} // end Parser
+
+
+public class Errors {
+	public int count = 0;                                    // number of errors detected
+	public System.IO.TextWriter errorStream = Console.Out;   // error messages go to this stream
+  public string errMsgFormat = "-- line {0} col {1}: {2}"; // 0=line, 1=column, 2=text
+  
+	public void SynErr (int line, int col, int n) {
+		string s;
+		switch (n) {
+			case 0: s = "EOF expected"; break;
+			case 1: s = "leq expected"; break;
+			case 2: s = "geq expected"; break;
+			case 3: s = "eq expected"; break;
+			case 4: s = "altleq expected"; break;
+			case 5: s = "altgeq expected"; break;
+			case 6: s = "plus expected"; break;
+			case 7: s = "minus expected"; break;
+			case 8: s = "times expected"; break;
+			case 9: s = "divide expected"; break;
+			case 10: s = "lparen expected"; break;
+			case 11: s = "rparen expected"; break;
+			case 12: s = "variable expected"; break;
+			case 13: s = "number expected"; break;
+			case 14: s = "??? expected"; break;
+			case 15: s = "invalid Constraint"; break;
+			case 16: s = "invalid Factor"; break;
+
+			default: s = "error " + n; break;
+		}
+		errorStream.WriteLine(errMsgFormat, line, col, s);
+		count++;
+	}
+
+	public void SemErr (int line, int col, string s) {
+		errorStream.WriteLine(errMsgFormat, line, col, s);
+		count++;
+	}
+	
+	public void SemErr (string s) {
+		errorStream.WriteLine(s);
+		count++;
+	}
+	
+	public void Warning (int line, int col, string s) {
+		errorStream.WriteLine(errMsgFormat, line, col, s);
+	}
+	
+	public void Warning(string s) {
+		errorStream.WriteLine(s);
+	}
+} // Errors
+
+
+public class FatalError: Exception {
+	public FatalError(string m): base(m) {}
+}
+
---
+++ Cassowary/Parsing/Scanner.cs
@@ -0,0 +1,354 @@
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+
+
+public class Token {
+	public int kind;    // token kind
+	public int pos;     // token position in the source text (starting at 0)
+	public int col;     // token column (starting at 0)
+	public int line;    // token line (starting at 1)
+	public string val;  // token value
+	public Token next;  // ML 2005-03-11 Tokens are kept in linked list
+}
+
+//-----------------------------------------------------------------------------------
+// Buffer
+//-----------------------------------------------------------------------------------
+public class Buffer {
+	public const int EOF = char.MaxValue + 1;
+	const int MAX_BUFFER_LENGTH = 64 * 1024; // 64KB
+	byte[] buf;         // input buffer
+	int bufStart;       // position of first byte in buffer relative to input stream
+	int bufLen;         // length of buffer
+	int fileLen;        // length of input stream
+	int pos;            // current position in buffer
+	Stream stream;      // input stream (seekable)
+	bool isUserStream;  // was the stream opened by the user?
+	
+	public Buffer (Stream s, bool isUserStream) {
+		stream = s; this.isUserStream = isUserStream;
+		fileLen = bufLen = (int) s.Length;
+		if (stream.CanSeek && bufLen > MAX_BUFFER_LENGTH) bufLen = MAX_BUFFER_LENGTH;
+		buf = new byte[bufLen];
+		bufStart = Int32.MaxValue; // nothing in the buffer so far
+		Pos = 0; // setup buffer to position 0 (start)
+		if (bufLen == fileLen) Close();
+	}
+	
+	protected Buffer(Buffer b) { // called in UTF8Buffer constructor
+		buf = b.buf;
+		bufStart = b.bufStart;
+		bufLen = b.bufLen;
+		fileLen = b.fileLen;
+		pos = b.pos;
+		stream = b.stream;
+		b.stream = null;
+		isUserStream = b.isUserStream;
+	}
+
+	~Buffer() { Close(); }
+	
+	protected void Close() {
+		if (!isUserStream && stream != null) {
+			stream.Close();
+			stream = null;
+		}
+	}
+	
+	public virtual int Read () {
+		if (pos < bufLen) {
+			return buf[pos++];
+		} else if (Pos < fileLen) {
+			Pos = Pos; // shift buffer start to Pos
+			return buf[pos++];
+		} else {
+			return EOF;
+		}
+	}
+
+	public int Peek () {
+		int curPos = Pos;
+		int ch = Read();
+		Pos = curPos;
+		return ch;
+	}
+	
+	public string GetString (int beg, int end) {
+		int len = end - beg;
+		char[] buf = new char[len];
+		int oldPos = Pos;
+		Pos = beg;
+		for (int i = 0; i < len; i++) buf[i] = (char) Read();
+		Pos = oldPos;
+		return new String(buf);
+	}
+
+	public int Pos {
+		get { return pos + bufStart; }
+		set {
+			if (value < 0) value = 0; 
+			else if (value > fileLen) value = fileLen;
+			if (value >= bufStart && value < bufStart + bufLen) { // already in buffer
+				pos = value - bufStart;
+			} else if (stream != null) { // must be swapped in
+				stream.Seek(value, SeekOrigin.Begin);
+				bufLen = stream.Read(buf, 0, buf.Length);
+				bufStart = value; pos = 0;
+			} else {
+				pos = fileLen - bufStart; // make Pos return fileLen
+			}
+		}
+	}
+}
+
+//-----------------------------------------------------------------------------------
+// UTF8Buffer
+//-----------------------------------------------------------------------------------
+public class UTF8Buffer: Buffer {
+	public UTF8Buffer(Buffer b): base(b) {}
+
+	public override int Read() {
+		int ch;
+		do {
+			ch = base.Read();
+			// until we find a uft8 start (0xxxxxxx or 11xxxxxx)
+		} while ((ch >= 128) && ((ch & 0xC0) != 0xC0) && (ch != EOF));
+		if (ch < 128 || ch == EOF) {
+			// nothing to do, first 127 chars are the same in ascii and utf8
+			// 0xxxxxxx or end of file character
+		} else if ((ch & 0xF0) == 0xF0) {
+			// 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+			int c1 = ch & 0x07; ch = base.Read();
+			int c2 = ch & 0x3F; ch = base.Read();
+			int c3 = ch & 0x3F; ch = base.Read();
+			int c4 = ch & 0x3F;
+			ch = (((((c1 << 6) | c2) << 6) | c3) << 6) | c4;
+		} else if ((ch & 0xE0) == 0xE0) {
+			// 1110xxxx 10xxxxxx 10xxxxxx
+			int c1 = ch & 0x0F; ch = base.Read();
+			int c2 = ch & 0x3F; ch = base.Read();
+			int c3 = ch & 0x3F;
+			ch = (((c1 << 6) | c2) << 6) | c3;
+		} else if ((ch & 0xC0) == 0xC0) {
+			// 110xxxxx 10xxxxxx
+			int c1 = ch & 0x1F; ch = base.Read();
+			int c2 = ch & 0x3F;
+			ch = (c1 << 6) | c2;
+		}
+		return ch;
+	}
+}
+
+//-----------------------------------------------------------------------------------
+// Scanner
+//-----------------------------------------------------------------------------------
+public class Scanner {
+	const char EOL = '\n';
+	const int eofSym = 0; /* pdt */
+	const int maxT = 14;
+	const int noSym = 14;
+
+
+	public Buffer buffer; // scanner buffer
+	
+	Token t;          // current token
+	int ch;           // current input character
+	int pos;          // byte position of current character
+	int col;          // column number of current character
+	int line;         // line number of current character
+	int oldEols;      // EOLs that appeared in a comment;
+	Dictionary<int, int> start; // maps first token character to start state
+
+	Token tokens;     // list of tokens already peeked (first token is a dummy)
+	Token pt;         // current peek token
+	
+	char[] tval = new char[128]; // text of current token
+	int tlen;         // length of current token
+	
+	public Scanner (string fileName) {
+		try {
+			Stream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read);
+			buffer = new Buffer(stream, false);
+			Init();
+		} catch (IOException) {
+			throw new FatalError("Cannot open file " + fileName);
+		}
+	}
+	
+	public Scanner (Stream s) {
+		buffer = new Buffer(s, true);
+		Init();
+	}
+	
+	void Init() {
+		pos = -1; line = 1; col = 0;
+		oldEols = 0;
+		NextCh();
+		if (ch == 0xEF) { // check optional byte order mark for UTF-8
+			NextCh(); int ch1 = ch;
+			NextCh(); int ch2 = ch;
+			if (ch1 != 0xBB || ch2 != 0xBF) {
+				throw new FatalError(String.Format("illegal byte order mark: EF {0,2:X} {1,2:X}", ch1, ch2));
+			}
+			buffer = new UTF8Buffer(buffer); col = 0;
+			NextCh();
+		}
+		start = new Dictionary<int, int>(128);
+		for (int i = 65; i <= 90; ++i) start[i] = 16;
+		for (int i = 97; i <= 122; ++i) start[i] = 16;
+		for (int i = 48; i <= 57; ++i) start[i] = 17;
+		start[60] = 1; 
+		start[62] = 3; 
+		start[61] = 5; 
+		start[91] = 6; 
+		start[93] = 8; 
+		start[43] = 10; 
+		start[45] = 11; 
+		start[42] = 12; 
+		start[47] = 13; 
+		start[40] = 14; 
+		start[41] = 15; 
+		start[Buffer.EOF] = -1;
+
+		pt = tokens = new Token();  // first token is a dummy
+	}
+	
+	void NextCh() {
+		if (oldEols > 0) { ch = EOL; oldEols--; } 
+		else {
+			pos = buffer.Pos;
+			ch = buffer.Read(); col++;
+			// replace isolated '\r' by '\n' in order to make
+			// eol handling uniform across Windows, Unix and Mac
+			if (ch == '\r' && buffer.Peek() != '\n') ch = EOL;
+			if (ch == EOL) { line++; col = 0; }
+		}
+
+	}
+
+	void AddCh() {
+		if (tlen >= tval.Length) {
+			char[] newBuf = new char[2 * tval.Length];
+			Array.Copy(tval, 0, newBuf, 0, tval.Length);
+			tval = newBuf;
+		}
+		tval[tlen++] = (char)ch;
+		NextCh();
+	}
+
+
+
+
+	void CheckLiteral() {
+		switch (t.val) {
+			default: break;
+		}
+	}
+
+	Token NextToken() {
+		while (ch == ' ' ||
+			ch >= 9 && ch <= 10 || ch == 13
+		) NextCh();
+
+		t = new Token();
+		t.pos = pos; t.col = col; t.line = line; 
+		int state;
+		try { state = start[ch]; } catch (KeyNotFoundException) { state = 0; }
+		tlen = 0; AddCh();
+		
+		switch (state) {
+			case -1: { t.kind = eofSym; break; } // NextCh already done
+			case 0: { t.kind = noSym; break; }   // NextCh already done
+			case 1:
+				if (ch == '=') {AddCh(); goto case 2;}
+				else {t.kind = noSym; break;}
+			case 2:
+				{t.kind = 1; break;}
+			case 3:
+				if (ch == '=') {AddCh(); goto case 4;}
+				else {t.kind = noSym; break;}
+			case 4:
+				{t.kind = 2; break;}
+			case 5:
+				{t.kind = 3; break;}
+			case 6:
+				if (ch == '=') {AddCh(); goto case 7;}
+				else {t.kind = noSym; break;}
+			case 7:
+				{t.kind = 4; break;}
+			case 8:
+				if (ch == '=') {AddCh(); goto case 9;}
+				else {t.kind = noSym; break;}
+			case 9:
+				{t.kind = 5; break;}
+			case 10:
+				{t.kind = 6; break;}
+			case 11:
+				{t.kind = 7; break;}
+			case 12:
+				{t.kind = 8; break;}
+			case 13:
+				{t.kind = 9; break;}
+			case 14:
+				{t.kind = 10; break;}
+			case 15:
+				{t.kind = 11; break;}
+			case 16:
+				if (ch >= '-' && ch <= '.' || ch >= '0' && ch <= '9' || ch >= 'A' && ch <= 'Z' || ch == '_' || ch >= 'a' && ch <= 'z') {AddCh(); goto case 16;}
+				else {t.kind = 12; break;}
+			case 17:
+				if (ch >= '0' && ch <= '9') {AddCh(); goto case 17;}
+				else if (ch == '.') {AddCh(); goto case 18;}
+				else {t.kind = 13; break;}
+			case 18:
+				if (ch >= '0' && ch <= '9') {AddCh(); goto case 18;}
+				else if (ch == 'E') {AddCh(); goto case 19;}
+				else {t.kind = 13; break;}
+			case 19:
+				if (ch >= '0' && ch <= '9') {AddCh(); goto case 21;}
+				else if (ch == '+' || ch == '-') {AddCh(); goto case 20;}
+				else {t.kind = noSym; break;}
+			case 20:
+				if (ch >= '0' && ch <= '9') {AddCh(); goto case 21;}
+				else {t.kind = noSym; break;}
+			case 21:
+				if (ch >= '0' && ch <= '9') {AddCh(); goto case 21;}
+				else {t.kind = 13; break;}
+
+		}
+		t.val = new String(tval, 0, tlen);
+		return t;
+	}
+	
+	// get the next token (possibly a token already seen during peeking)
+	public Token Scan () {
+		if (tokens.next == null) {
+			return NextToken();
+		} else {
+			pt = tokens = tokens.next;
+			return tokens;
+		}
+	}
+
+	// peek for the next token, ignore pragmas
+	public Token Peek () {
+		if (pt.next == null) {
+			do {
+				pt = pt.next = NextToken();
+			} while (pt.kind > maxT); // skip pragmas
+		} else {
+			do {
+				pt = pt.next;
+			} while (pt.kind > maxT);
+		}
+		return pt;
+	}
+	
+	// make sure that peeking starts at the current scan position
+	public void ResetPeek () { pt = tokens; }
+
+} // end Scanner
+
